65127ac6c2fe923edb0061be30d4946212f8547f
Ryely <ryely.moore@outlook.com>
2021-08-30 04:15:03 -0600

    filled out other routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -61,7 +61,7 @@ router.put('/:id', (req, res) => {
   })
   .then(dbCatData => {
     if (!dbCatData) {
-      res.status(404).json({ message: 'No post found with this id' });
+      res.status(404).json({ message: 'No category found with this id' });
       return;
     }
     res.json(dbCatData);
@@ -81,7 +81,7 @@ router.delete('/:id', (req, res) => {
   })
   .then(dbCatData => {
     if (!dbCatData) {
-      res.status(404).json({ message: 'No post found with this id' });
+      res.status(404).json({ message: 'No category found with this id' });
       return;
     }
     res.json(dbCatData);
